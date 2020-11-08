915fb407356cf084c81df4a7dbb44ecf8ea45c5d
Bhadresh Savani <bhadreshpsavani@gmail.com>
2020-11-08 14:15:08 +0530

    set basic project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import react from 'react'
+import Chatbot from 'react-chatbot-kit'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+import ActionProvider from './ActionProvider'
+import MessageParser from './MessageParser'
+import Config from './Config'
+
+function App(){
+  return(
+    <div className='App'>
+      <header className='App-header'>
+        <Chatbot config={Config} actionProvider={ActionProvider} messageParser={MessageParser}/>
       </header>
     </div>
   );
 }
-
 export default App;
